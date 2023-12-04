1b09fca1cfb68398a22d0154e56d4e712e3b24db
Bdnature <84246065+Quddos@users.noreply.github.com>
2023-12-04 01:39:14 -0800

    new

--- lib/pages/welcome/welcome.dart
+++ lib/pages/welcome/welcome.dart
@@ -1,4 +1,5 @@
 import 'package:dots_indicator/dots_indicator.dart';
+import 'package:edy_learner/fake_main.dart';
 import 'package:edy_learner/pages/welcome/bloc/welcome_blocs.dart';
 import 'package:edy_learner/pages/welcome/bloc/welcome_events.dart';
 import 'package:edy_learner/pages/welcome/bloc/welcome_states.dart';
@@ -15,143 +16,134 @@ class Welcome extends StatefulWidget {
 }
 
 class _WelcomeState extends State<Welcome> {
+  PageController pageController = PageController(initialPage: 0);
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.blue,
-      child: Scaffold(
-        body: BlocBuilder<WelcomeBloc,WelcomeState>(
-          builder: (context, state){
-            return Container(
-              margin: EdgeInsets.only(top:34.h),
-              width: 375.w,
-              child: Stack(
-                alignment: Alignment.topCenter,
-                children: [
-                  PageView(
-                    onPageChanged: (index){
-
-                      state.page= index;
-                      BlocProvider.of<WelcomeBloc>(context).add(WelcomeEvent());
-
-
-                    },
-                    children: [
-                      _page(
-                          1,
-                          context,
-                          "next",
-                          "first see Learning",
-                          "Forget about troubles underneath physical study",
-                          "assets/images/reading.png"
-                      ),
-                      _page(
-                          2,
-                          context,
-                          "next",
-                          "Cnnect With Everyone",
-                          "Always keep in touch with your tutors, and friends, Let's get Connected",
-                          "assets/images/boy.png"
-                      ),
-                      _page(
-                          3,
-                          context,
-                          "get started",
-                          "Always Fascinated Learning",
-                          "Anytime, anywhere, the time is as your discretion to study whenever you want",
-                          "assets/images/man.png"
-                      ),
-
-                    ],
+      child: Scaffold(body: BlocBuilder<WelcomeBloc, WelcomeState>(
+        builder: (context, state) {
+          return Container(
+            margin: EdgeInsets.only(top: 34.h),
+            width: 375.w,
+            child: Stack(
+              alignment: Alignment.topCenter,
+              children: [
+                PageView(
+                  controller: pageController,
+                  onPageChanged: (index) {
+                    state.page = index;
+                    BlocProvider.of<WelcomeBloc>(context).add(WelcomeEvent());
+                  },
+                  children: [
+                    _page(
+                        1,
+                        context,
+                        "next",
+                        "first see Learning",
+                        "Forget about troubles underneath physical study",
+                        "assets/images/reading.png"),
+                    _page(
+                        2,
+                        context,
+                        "next",
+                        "Cnnect With Everyone",
+                        "Always keep in touch with your tutors, and friends, Let's get Connected",
+                        "assets/images/boy.png"),
+                    _page(
+                        3,
+                        context,
+                        "get started",
+                        "Always Fascinated Learning",
+                        "Anytime, anywhere, the time is as your discretion to study whenever you want",
+                        "assets/images/man.png"),
+                  ],
+                ),
+                Positioned(
+                  bottom: 100.h,
+                  child: DotsIndicator(
+                    position: state.page,
+                    dotsCount: 3,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    decorator: DotsDecorator(
+                        color: Colors.grey,
+                        activeColor: Colors.blue,
+                        size: const Size.square(8.0),
+                        activeSize: const Size(18.0, 8.0),
+                        activeShape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5.0),
+                        )),
                   ),
-                  Positioned(
-                    bottom: 100.h,
-                    child:
-                    DotsIndicator(
-                      position: state.page,
-                      dotsCount: 3,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      decorator: DotsDecorator(
-                          color: Colors.grey,
-                          activeColor: Colors.blue,
-                          size: const Size.square(8.0),
-                          activeSize: const Size(18.0, 8.0),
-                          activeShape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(5.0),
-                          )
-                      ),
-
-                    ),
-                  )
-                ],
-              ),
-
-            );
-          },
-        )
-      ),
+                )
+              ],
+            ),
+          );
+        },
+      )),
     );
   }
 
-  Widget _page(int index, BuildContext context, String buttonName, String title, String subTile, String imagePath){
-    return   Column(
+  Widget _page(int index, BuildContext context, String buttonName, String title,
+      String subTile, String imagePath) {
+    return Column(
       children: [
         SizedBox(
           width: 345.w,
           height: 345.w,
-          child: Image.asset(imagePath,
-          fit:BoxFit.cover),
+          child: Image.asset(imagePath, fit: BoxFit.cover),
         ),
         Container(
-          child: Text(
-              title,
+          child: Text(title,
               style: TextStyle(
                 color: Colors.black,
                 fontSize: 24.sp,
                 fontWeight: FontWeight.normal,
-              )
-          ),
+              )),
         ),
         Container(
           width: 337.w,
-          padding: EdgeInsets.only(left: 30.w,right: 30.w),
-          child: Text(
-              subTile,
+          padding: EdgeInsets.only(left: 30.w, right: 30.w),
+          child: Text(subTile,
               style: TextStyle(
                 color: Colors.black.withOpacity(0.5),
                 fontSize: 14.sp,
                 fontWeight: FontWeight.normal,
-              )
-          ),
+              )),
         ),
-        Container(
-          margin: EdgeInsets.only(top: 100.h, left: 25.w,right: 25.w),
-          width: 325.w,
-          height: 50.h,
-          decoration: BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.all(Radius.circular(15.w)),
-              boxShadow: [
-                BoxShadow(
-                    color: Colors.grey.withOpacity(0.1),
-                    spreadRadius: 1,
-                    blurRadius: 2,
-                    offset: Offset(0,1)
-                ),
-
-              ]
-          ),
-          child: Center(
-            child: Text(
-                buttonName,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 16.sp,
-                  fontWeight: FontWeight.normal,
-                )
+        GestureDetector(
+          onTap: () {
+            if (index < 3) {
+              pageController.animateToPage(index,
+                  duration: const Duration(milliseconds: 1000),
+                  curve: Curves.easeIn);
+            }
+            else{
+              Navigator.of(context).push(MaterialPageRoute(builder: (context)=>MyHomePage()));}
+          },
+          child: Container(
+            margin: EdgeInsets.only(top: 100.h, left: 25.w, right: 25.w),
+            width: 325.w,
+            height: 50.h,
+            decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.all(Radius.circular(15.w)),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.grey.withOpacity(0.1),
+                      spreadRadius: 1,
+                      blurRadius: 2,
+                      offset: Offset(0, 1)),
+                ]),
+            child: Center(
+              child: Text(buttonName,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16.sp,
+                    fontWeight: FontWeight.normal,
+                  )),
             ),
           ),
-        )
+        ),
       ],
     );
   }
